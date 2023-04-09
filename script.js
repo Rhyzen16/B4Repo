dc8e42b09d05d64a985a9f9343588d00ae6c2f3d
Rhyzen16 <102942367+Rhyzen16@users.noreply.github.com>
2023-04-09 14:49:26 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function() {
+
+  // When the form is submitted
+
+  $(".login-form").submit(function(e) {
+
+    e.preventDefault(); // Prevent default form submission
+
+    var username = $("#username").val();
+
+    var password = $("#password").val();
+
+    // TODO: Add login logic here
+
+  });
+
+});
+
